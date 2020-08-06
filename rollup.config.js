0fed731b8499fbba9bfcebd444dfceb65eebf8e0
Bo <ybly.1993@gmail.com>
2020-08-06 14:04:37 +0100

    add babel and resolve configuration to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
+import babel from '@rollup/plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+
 export default {
     input: 'src/index.js',
     output: {
         file: 'dist/bundle.js',
         format: 'esm'
-    }
+    },
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**'
+        })
+    ]
 }
